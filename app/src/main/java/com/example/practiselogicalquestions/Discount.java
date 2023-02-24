e447255fd12f07c71030c4dcc8f402f3b982ff21
roopa <roopa.peddinti@improve10x.com>
2023-02-24 10:28:03 +0530

    completed divisible by 15 activity

---
+++ app/src/main/java/com/example/practiselogicalquestions/Discount.java
@@ -0,0 +1,14 @@
+package com.example.practiselogicalquestions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Discount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discount);
+    }
+}
